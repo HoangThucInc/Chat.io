d891cd55c94cf22274cce0050943982e1343f2cb
Hoang Thuc <hoangthuc701@gmail.com>
2018-10-02 16:12:17 +0700

    My app
    App write Nodejs

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app= express();
+var indexRouter = require('./routers/index.js');
+
+app.set('view engine','ejs');
+app.set('views','./views/');
+app.use(express.static('public'));
+//app.use('/',indexRouter);
+var server = require('http').Server(app);
+var io= require('socket.io')(server);
+
+var Users=[""];
+io.on("connection", function (socket){
+    //console.log("Co nguoi ket noi: "+socket.id);
+    socket.on("disconnect", function (){
+        console.log(socket.id +" ngat ket noi");
+    });
+    socket.on("client-send-username", function (data){
+       if (Users.indexOf(data)>=0) {
+           socket.emit("register-fail");
+       } else{
+           Users.push(data);
+           socket.username=data;
+           socket.emit("register-success",data);
+           io.sockets.emit("send-arr-user",Users);
+       }
+    });
+
+    socket.on("user-logout", function (){
+        socket.emit("logout-success");
+        Users.splice(Users.indexOf(socket.username),1);
+        socket.broadcast.emit("send-arr-user",Users);
+    });
+    socket.on("user-send-message", function (data){
+        io.sockets.emit("server-send-message",{name: socket.username, mess:data});
+    });
+
+});
+server.listen(3000);
+app.get('/', function (req,res) {
+    res.render("index");
+})
---
+++ public/xuli.js
@@ -0,0 +1,39 @@
+var socket = io("http://localhost:3000");
+socket.on("register-fail", function (){
+    alert("This username has already used.");
+});
+socket.on("register-success", function (data){
+    alert("You has registered successfully");
+    $("#currentUser").html(data);
+    $("#login-form").hide(2000);
+    $("#chat-form").show(1000);
+});
+socket.on("send-arr-user", function (data){
+    $("#box-content").html("");
+    data.forEach(function(i){
+        $("#box-content").append("<div class='userOnline'>"+i +"</div>")
+    });
+});
+socket.on("logout-success", function (){
+    alert("You have successfully logout.");
+    $("#chat-form").hide(2);
+    $("#login-form").show(1);
+    $("#textMessage").val("");
+    $("#listMessages").val("");
+});
+socket.on("server-send-message", function(data){
+    $("#listMessages").append(data.name+"    :    "+ data.mess+"\r\n");
+});
+$(document).ready(function (){
+    $("#login-form").show();
+    $("#chat-form").hide();
+    $("#btnregister").click(function(){
+        socket.emit("client-send-username",$("#username").val());
+    });
+    $("#btn-logout").click(function(){
+        socket.emit("user-logout");
+    });
+    $("#btnSend").click(function(){
+        socket.emit("user-send-message",$("#textMessage").val());
+    });
+});
